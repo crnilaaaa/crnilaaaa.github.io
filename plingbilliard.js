e5ba6f1c2ec8e9d3666be3ba11e5e5a9807be759
crnilaaaa <88246082+crnilaaaa@users.noreply.github.com>
2021-08-23 17:08:59 +0200

    Update plingbilliard.js

--- plingbilliard.js
+++ plingbilliard.js
@@ -26,70 +26,62 @@ var note =
 
   /* C major: 48 50 52 53 55 57 59 60
              0  2  4  5  7  9 11 12 */
-function setUpNoteGrid(root = 48, step = 1) {
-  note[0] = root; // C3
-  for (var y = 0, i = 0; y < 8; ++y) {
-    for (var x = 1; x < 8; ++x) {    
-      note[i] = note[i-1] + step;
-      switch(note[i] % 12) {
-        case 1:
-        case 3:
-        case 6:
-        case 8:
-        case 10:
-          note[i]++;
-        default: 
-          break;
-      }
+  
+  function setUpNoteGrid() {
+    var scale = (step = 1) => {
+      let i = 0;
+      return ["C", "D", "E", "F", "G", "A", "B"][i++%8];
+    }
+    for(var i = 0; i < 64; ++i) {
+       note[i] = scale() + "4"; 
     }
   }
-}
 
-setUpNoteGrid();
-console.log(note);
+  setUpNoteGrid();
+  console.log(note);
   
-var green = (btn) => { output.send(0x90, [btn, light_state[btn] = 1]); };
-var red = (btn) => { output.send(0x90, [btn, light_state[btn] = 2]); };
-var toggleblink = (btn) => { output.send(0x90, btn, Math.abs(light_state[btn] - 4)); };
-var off = (btn) => { output.send(0x90, [btn, 0]); };
+  var green = (btn) => { output.send(0x90, [btn, light_state[btn] = 1]); };
+  var red = (btn) => { output.send(0x90, [btn, light_state[btn] = 2]); };
+  var toggleblink = (btn) => { output.send(0x90, btn, Math.abs(light_state[btn] - 4)); };
+  var off = (btn) => { output.send(0x90, [btn, 0]); };
 
-  
-function applyWorld() { 
-    /*
-    var siteState = [];
-    for (var i = 0; i < 64; ++i) {
-      siteState[i] = document.getElementById("checkbox" + i).checked; 
-    }
-    */
-}
-/*
-const click = new Tone.Loop(time => {
-  new Tone.FMSynth().toDestination().triggerAttackRelease(myNote, "8n", time); // debug
-  applyWorld();
-}, "4n").start(0);
-*/
-  input.addListener('noteoff', 'all', 
-    function(e) { 
-      off(e.data[1]); 
-      if (e.data[1] < 64) { 
-        synth.triggerRelease(note[e.data[1]]);
-        console.log("stopping " + note[e.data[1]] + " triggered by " + e.data[1] );
+
+  function applyWorld() { 
+      /*
+      var siteState = [];
+      for (var i = 0; i < 64; ++i) {
+        siteState[i] = document.getElementById("checkbox" + i).checked; 
       }
-  });
-  input.addListener('noteon', 'all',
-    function(e) {
-      var vv = e.data[1];
-      var ll = light_state;
-      if (vv == 98) 
-        allOff();
-      else if (vv < 64) {
-        green(vv);
-        document.getElementById("checkbox" + vv).checked = ll[vv] != 0 ? true : false; // ??  
-        synth.triggerAttack(note[vv]);
-        console.log("playing " + note[vv] + " triggered by " + vv );
+      */
+  }
+  /*
+  const click = new Tone.Loop(time => {
+    new Tone.FMSynth().toDestination().triggerAttackRelease(myNote, "8n", time); // debug
+    applyWorld();
+  }, "4n").start(0);
+  */
+    input.addListener('noteoff', 'all', 
+      function(e) { 
+        off(e.data[1]); 
+        if (e.data[1] < 64) { 
+          synth.triggerRelease(note[e.data[1]]);
+          console.log("stopping " + note[e.data[1]] + " triggered by " + e.data[1] );
+        }
+    });
+    input.addListener('noteon', 'all',
+      function(e) {
+        var vv = e.data[1];
+        var ll = light_state;
+        if (vv == 98) 
+          allOff();
+        else if (vv < 64) {
+          green(vv);
+          document.getElementById("checkbox" + vv).checked = ll[vv] != 0 ? true : false; // ??  
+          synth.triggerAttack(note[vv]);
+          console.log("playing " + note[vv] + " triggered by " + vv );
+        }
       }
-    }
-  );
+    );
 });
 
 function doStart() {
