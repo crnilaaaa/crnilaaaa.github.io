36e6acace3a47f29d6d90295e380b76797f0b323
crnilaaaa <88246082+crnilaaaa@users.noreply.github.com>
2021-08-23 18:37:55 +0200

    Update plingbilliard.js

--- plingbilliard.js
+++ plingbilliard.js
@@ -13,7 +13,7 @@ WebMidi.enable(function(err) {
       document.getElementById("checkbox" + i).indeterminate = false; // ??
     }
   };
- 
+  
   var octaveRange = 0;
   var noteStep = 1;
   var note = 
@@ -56,7 +56,8 @@ WebMidi.enable(function(err) {
   var red = (btn) => { output.send(0x90, [btn, light_state[btn] = 2]); };
   var toggleblink = (btn) => { output.send(0x90, btn, Math.abs(light_state[btn] - 4)); };
   var off = (btn) => { output.send(0x90, [btn, 0]); };
-  var togglecheck = (ck) => { document.getElementById("checkbox" + ck).checked
+  var togglecheck = (ck) => {  document.getElementById("checkbox" + ck).checked =
+    document.getElementById("checkbox" + ck).checked ? false : true; };
 
   function applyWorld() { 
       /*
@@ -72,6 +73,7 @@ WebMidi.enable(function(err) {
       off(e.data[1]); 
       if (e.data[1] < 64) { 
         synth.triggerRelease(note[e.data[1]]);
+        togglecheck(vv);
         // console.log("stopping " + note[e.data[1]] + " triggered by " + e.data[1] );
       }
   });
@@ -84,6 +86,7 @@ WebMidi.enable(function(err) {
         allOff();
       else if (vv < 64) {
         green(vv);
+        togglecheck(vv);
         synth.triggerAttack(note[vv]);
         // console.log("playing " + note[vv] + " triggered by " + vv );
       }
