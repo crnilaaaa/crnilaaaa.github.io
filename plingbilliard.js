eda8b10ff8c79bc9348a95b1c93094709bbbac42
crnilaaaa <88246082+crnilaaaa@users.noreply.github.com>
2021-08-23 16:13:02 +0200

    Update plingbilliard.js

--- plingbilliard.js
+++ plingbilliard.js
@@ -1,3 +1,5 @@
+const synth = new OmniOscillator();
+
 WebMidi.enable(function(err) {
   if(err) {
     console.log("blerg", err);
@@ -12,36 +14,62 @@ WebMidi.enable(function(err) {
       document.getElementById("checkbox" + i).indeterminate = false; // ??
     }
   };
+ 
+var xstep = 0;
+var ystep = 0;
+var note = 
+ [0, 0, 0, 0, 0, 0, 0, 0, 
+  0, 0, 0, 0, 0, 0, 0, 0, 
+  0, 0, 0, 0, 0, 0, 0, 0, 
+  0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 
+  0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 
+  0, 0, 0, 0, 0, 0, 0, 0];
 
-  var noteMap = [
-    "C4,", "E4,", "G4,", "B4,", "C4,", "E4,", "G4,", "B4,", 
-    "D4,", "F4,", "G4,", "B4,", "D4,", "F4,", "G4,", "B4,",
-    "A4,", "C4,", "E4,", "B4,", "A4,", "C4,", "E4,", "B4,",
-    "E4,", "F4,", "A4,", "C4,", "E4,", "F4,", "A4,", "F4,",
-    "G4,", "B4,", "C4,", "E4,", "G4,", "B4,", "C4,", "E4,",
-    "B4,", "D4,", "F4,", "G4,", "B4,", "D4,", "F4,", "G4,",
-    "E4,", "B4,", "A4,", "C4,", "E4,", "B4,", "A4,", "C4,",
-    "C4,", "E4,", "F4,", "A4,", "C4,", "E4,", "F4,", "A4,", 
-];
+  /* C major: 48 50 52 53 55 57 59 60
+             0  2  4  5  7  9 11 12 */
+function setUpNoteGrid(root = 48, step = 1) {
+  note[0] = root; // C3
+  for (var y = 0, i = 0; y < 8; ++y) {
+    for (var x = 1; x < 8; ++x) {
+      i = x + x * y;
+      note[i] = note[i-1] + xstep;
+      note[i] = note[i] % 12;
+      switch(note[i]) {
+        case 1:
+        case 3:
+        case 6:
+        case 8:
+        case 10:
+          note[i]++;
+        default: break;
+      }
+    }
+    note[i] = note[i] + ystep;
+  }
+}
   
-  var green = (btn) => { output.send(0x90, [btn, light_state[btn] = 1]); };
-  var red = (btn) => { output.send(0x90, [btn, light_state[btn] = 2]); };
-  var toggleblink = (btn) => { output.send(0x90, btn, Math.abs(light_state[btn] - 4)); };
+var green = (btn) => { output.send(0x90, [btn, light_state[btn] = 1]); };
+var red = (btn) => { output.send(0x90, [btn, light_state[btn] = 2]); };
+var toggleblink = (btn) => { output.send(0x90, btn, Math.abs(light_state[btn] - 4)); };
+var off = (btn) => { output.send(0x90, [btn, 0]); };
   
-  function applyWorld() { 
+function applyWorld() { 
     /*
     var siteState = [];
     for (var i = 0; i < 64; ++i) {
       siteState[i] = document.getElementById("checkbox" + i).checked; 
     }
     */
-  }
-  var myNote;
-  const click = new Tone.Loop(time => {
-    new Tone.FMSynth().toDestination().triggerAttackRelease(myNote, "8n", time); // debug
-    applyWorld();
-  }, "4n").start(0);
-    
+}
+/*
+const click = new Tone.Loop(time => {
+  new Tone.FMSynth().toDestination().triggerAttackRelease(myNote, "8n", time); // debug
+  applyWorld();
+}, "4n").start(0);
+*/
+  input.addListener('noteoff', 'all', function(e) { off(e.data[1]); });
   input.addListener('noteon', 'all',
     function(e) {
       var vv = e.data[1];
@@ -50,10 +78,8 @@ WebMidi.enable(function(err) {
       if (vv == 98) 
         allOff();
       else if (vv < 64) {
-        allOff();
         green(vv);
-        document.getElementById("checkbox" + vv).checked = ll[vv] != 0 ? true : false; // ??
-        myNote = noteMap[vv];
+        document.getElementById("checkbox" + vv).checked = ll[vv] != 0 ? true : false; // ??      
       }
     }
   );
