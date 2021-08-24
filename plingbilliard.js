47887c16c241305ba6033f4997faf5d859316405
crnilaaaa <88246082+crnilaaaa@users.noreply.github.com>
2021-08-24 03:01:05 +0200

    Update plingbilliard.js

--- plingbilliard.js
+++ plingbilliard.js
@@ -75,7 +75,7 @@ WebMidi.enable(function(err) {
     }
     
     note[0] = "C4";
-    document.getElementById("textbox" +i).value = note[i];
+    document.getElementById("textbox0").value = note[i];
     for(var i = 1; i < 64; ++i) {
       note[i] = scale(step) + octave;
       document.getElementById("textbox" + i).value = note[i];
